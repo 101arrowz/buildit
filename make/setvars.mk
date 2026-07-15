36f88bc3de6b3868916ac20c9427b200569f24ab
Arjun Barrett <arjunbarrett@gmail.com>
2026-07-15 18:56:44 +0000

    fix

--- make/setvars.mk
+++ make/setvars.mk
@@ -28,8 +28,8 @@ DEPS_LIST+=$(BUILD_DIR)/d2x.dep
 endif
 
 # Create CFLAGS, LINKER_FLAGS, CFLAGS_INTERNAL and INCLUDE_FLAGS based on config
-CFLAGS_INTERNAL=-std=c++11 -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wmissing-declarations 
-CFLAGS_INTERNAL+=-Woverloaded-virtual -Wno-deprecated -Wdelete-non-virtual-dtor -Werror -Wno-vla -pedantic-errors 
+CFLAGS_INTERNAL=-std=c++11 -fPIC -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wmissing-declarations 
+CFLAGS_INTERNAL+=-Woverloaded-virtual -Wno-deprecated -Wdelete-non-virtual-dtor -Werror -Wno-vla -pedantic-errors
 CFLAGS=
 LINKER_FLAGS+=-L$(BUILD_DIR)/ -l$(LIBRARY_NAME)
 INCLUDE_FLAGS=-I$(INCLUDE_DIR) -I$(BUILD_DIR)/gen_headers/
